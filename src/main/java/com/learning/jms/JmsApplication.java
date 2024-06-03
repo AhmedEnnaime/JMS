71f370a9565fedb1cc30c5db4b6c975bafb649dd
AhmedEnnaime <ahmedennaime20@gmail.com>
2024-06-03 12:34:10 +0100

    created a pojo hello world and created an active mq embedeed server

--- src/main/java/com/learning/jms/JmsApplication.java
+++ src/main/java/com/learning/jms/JmsApplication.java
@@ -1,12 +1,22 @@
 package com.learning.jms;
 
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JmsApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		ActiveMQServer activeMQServer = ActiveMQServers.newActiveMQServer(
+				new ConfigurationImpl().setPersistenceEnabled(true)
+						.setJournalDirectory("target/data/journal")
+						.setSecurityEnabled(false)
+						.addAcceptorConfiguration("invm", "vm://0")
+		);
+		activeMQServer.start();
 		SpringApplication.run(JmsApplication.class, args);
 	}
 
---
+++ src/main/java/com/learning/jms/models/HelloWorldMessage.java
@@ -0,0 +1,19 @@
+package com.learning.jms.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+    private static final long serialVersionUID = 42L;
+    private UUID id;
+    private String message;
+}
